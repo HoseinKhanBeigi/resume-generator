cef8c6ee7bb65f60bb9d79c55bf49450831c9a2d
MAC <mac@MACs-iMac.local>
2023-12-09 00:52:49 +0330

    first commit

---
+++ src/pages/root/index.jsx
@@ -0,0 +1,96 @@
+import "./root.css";
+export default function Root() {
+  return (
+    <>
+      <div className="container">
+        <div className="header">
+          <div>
+            <div>hossein khan beigi</div>
+          </div>
+          <div>
+            <div>hoseinkhanbeigi@gmail.com</div>
+          </div>
+        </div>
+        <div className="section">
+          <div>
+            <div className="boldspan">Skills</div>
+            <div className="line"></div>
+          </div>
+          javascript - typescript - react - nextjs - vue - angular - redux -
+          graphql - service worker/PWA - nodejs - nestjs - git - docker -
+          nosql/sql - mongodb - micro-front - webassembly - mysql - mui - antd -
+          animejs - gsap - unit-test - cypress
+        </div>
+        <div className="boldspan">Experience</div>
+
+        <div className="section">
+          <div className="company-date">
+            <div className="bold"> Sr FrontEnd Developer</div>
+            <div className="bold">Round Table Apps</div>
+            <div>Sydney NSW </div>
+            <div className="bold">05/2017 to 02/2020</div>
+          </div>
+          <div>
+            {`coded and maintained React codebase for multiple high-traffic web applications.`}
+          </div>
+          <div className="achivments">
+            <div>
+              {`Drove a 25% improvement in website performance by optimizing critical rendering paths, reducing page load times, and implementing lazy loading techniques.`}
+            </div>
+          </div>
+        </div>
+        <div className="section">
+          <div className="company-date">
+            <div className="bold"> Sr FrontEnd Developer</div>
+            <div className="bold">Round Table Apps</div>
+            <div>Sydney NSW </div>
+            <div className="bold">05/2017 to 02/2020</div>
+          </div>
+          <div>
+            {`coded and maintained React codebase for multiple high-traffic web applications.`}
+          </div>
+          <div className="achivments">
+            <div>
+              {`Drove a 25% improvement in website performance by optimizing critical rendering paths, reducing page load times, and implementing lazy loading techniques.`}
+            </div>
+          </div>
+        </div>
+        <div className="section">
+          <div className="company-date">
+            <div className="bold"> Sr FrontEnd Developer</div>
+            <div className="bold">Round Table Apps</div>
+            <div>Sydney NSW </div>
+            <div className="bold">05/2017 to 02/2020</div>
+          </div>
+          <div>
+            {`My tenure at Round Table Apps not only honed my technical expertise across a diverse range of front-end technologies but also cultivated a collaborative and innovative approach to project delivery. I am eager to bring my skills and experiences to a new challenge where I can contribute to creating exceptional user experiences and driving technological innovation.`}
+          </div>
+          <div className="achivments">
+            <div>
+              {`Drove a 25% improvement in website performance by optimizing critical rendering paths, reducing page load times, and implementing lazy loading techniques.`}
+            </div>
+            <div>
+              {`Collaborated closely with cross-functional teams, including designers, back-end developers, and project managers, to deliver high-quality web solutions within tight deadlines. (Percentage of projects delivered on time: 95%)`}
+            </div>
+          </div>
+        </div>
+        <div className="section">
+          <div className="company-date">
+            <div className="bold"> Sr FrontEnd Developer</div>
+            <div className="bold">Datis Pars Java Solutions Co</div>
+            <div>Tehran - Munich</div>
+            <div className="bold">05/2015 to 05/2017</div>
+          </div>
+          <div className="achivments">
+            <div>
+              {`Developed and implemented complex JavaScript applications to improve website functionality and user experience.
+resolve technical issues, resulting in improved system performance.
+Optimized code to enhance site speed, reducing load times by up to 50% for end-users.
+Conducted unit tests and feature tests using jest for optimal performance and security.`}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
